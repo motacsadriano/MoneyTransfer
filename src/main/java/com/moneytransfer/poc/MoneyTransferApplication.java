85800e61cb3cecc786521f7078a487e0b0ae9986
Adriano Mota <motacsadriano@gmail.com>
2020-03-27 23:07:56 -0300

    Removing BasicErrorControlle from Swagger UI

--- src/main/java/com/moneytransfer/poc/MoneyTransferApplication.java
+++ src/main/java/com/moneytransfer/poc/MoneyTransferApplication.java
@@ -2,7 +2,14 @@ package com.moneytransfer.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,4 +20,12 @@ public class MoneyTransferApplication {
 		SpringApplication.run(MoneyTransferApplication.class, args);
 	}
 
+	@Bean
+	public Docket demoApi() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	            .select()
+	            .apis(RequestHandlerSelectors.any())
+	            .paths(Predicates.not(PathSelectors.regex("/error.*")))
+	            .build();
+	}
 }
